78b07c71faaae80e2df641531ad78721f46721ce
Christopher <christopherd1682@gmail.com>
2024-12-06 15:30:39 +0000

    Revert "Cognito support (#1)" (#4)
    This reverts commit 9292ee8b7072f8971380ae3c803ffc2f563336f5.

--- src/main/java/com/example/springbootgithubactiondemo/user/LoginRequest.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootgithubactiondemo.user;
-
-public class LoginRequest extends User{
-    public LoginRequest(String username, String password) {
-        super(username, password);
-    }
-}
