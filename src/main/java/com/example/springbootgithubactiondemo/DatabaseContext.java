b34891a545a37d9a9a08ab5742925d458303eed0
Christopher <christopherd1682@gmail.com>
2024-12-06 07:30:04 -0800

    Revert "Cognito support (#1)"
    This reverts commit 9292ee8b7072f8971380ae3c803ffc2f563336f5.

--- src/main/java/com/example/springbootgithubactiondemo/DatabaseContext.java
+++ src/main/java/com/example/springbootgithubactiondemo/DatabaseContext.java
@@ -1,10 +1,7 @@
 package com.example.springbootgithubactiondemo;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import org.bson.UuidRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,11 +20,6 @@ public class DatabaseContext extends AbstractMongoClientConfiguration {
 
     @Bean
     public MongoClient mongoClient() {
-        MongoClientSettings clientSettings =
-                MongoClientSettings.builder()
-                        .uuidRepresentation(UuidRepresentation.STANDARD)
-                        .applyConnectionString(new ConnectionString("mongodb://" + databaseProperties.getHost() + ":" + databaseProperties.getPort()))
-                        .build();
-        return MongoClients.create(clientSettings);
+        return MongoClients.create("mongodb://" + databaseProperties.getHost() + ":" + databaseProperties.getPort());
     }
 }
--- src/main/java/com/example/springbootgithubactiondemo/controller/HomeController.java
+++ src/main/java/com/example/springbootgithubactiondemo/controller/HomeController.java
@@ -1,23 +1,13 @@
 package com.example.springbootgithubactiondemo.controller;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.RSAKeyProvider;
-import com.example.springbootgithubactiondemo.AwsCognitoRSAKeyProvider;
 import com.example.springbootgithubactiondemo.DatabaseConfig;
-import com.example.springbootgithubactiondemo.ListUserPools;
-import com.example.springbootgithubactiondemo.LoginUser;
-import com.example.springbootgithubactiondemo.repository.UserRepository;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.bson.Document;
 
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
-import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,32 +22,17 @@ public class HomeController {
     private final MongoClient mongoClient;
 
     @Autowired
-    private final UserRepository userRepository;
-
-    @Autowired
-    private final CognitoIdentityProviderClient cognitoClient;
-
-    @Autowired
-    private Environment env;
-
-    @Autowired
-    public HomeController(MongoClient mongoClient, UserRepository userRepository, CognitoIdentityProviderClient cognitoClient) {
+    public HomeController(MongoClient mongoClient) {
         this.mongoClient = mongoClient;
-        this.userRepository = userRepository;
-        this.cognitoClient = cognitoClient;
     }
 
     @Autowired
     private DatabaseConfig databaseProperties;
 
-    @GetMapping("verify")
-    public String index(@RequestParam String token){
-        RSAKeyProvider keyProvider = new AwsCognitoRSAKeyProvider("us-west-2", "us-west-2_Ck5A798lj");
-        Algorithm algorithm = Algorithm.RSA256(keyProvider);
-        JWTVerifier jwtVerifier = JWT.require(algorithm)
-                .build();
 
-        return jwtVerifier.verify(token).getClaim("sub").asString();
+    @GetMapping("home")
+    public String index(){
+        return "OneBased Backend Service home endpoint running as expected.";
     }
 
     @Operation(
@@ -71,17 +46,5 @@ public class HomeController {
         List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
         databases.forEach(db -> System.out.println(db.toJson()));
         return "Received data: " + data;
-    }
-
-    @Operation(
-            summary = "Login",
-            description = "Authenticate to SOPHIE 2.0"
-    )
-    @PostMapping("/login")
-    public String loginUser(@RequestParam String username, @RequestParam String password) {
-        String userPoolClientId = "4t08ueomq1j8ava2ehnh1vujlt";
-        String userPoolClientSecret = env.getProperty("USER_POOL_SECRET");
-        System.out.println(username);
-        return LoginUser.initiateAuth(cognitoClient, userPoolClientId, userPoolClientSecret, username, password);
     }
 }
